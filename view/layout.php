f953e04440b96bf2984dfcc1193581f4ac8b0d10
Laura <Laura@DESKTOP-CN46QPV>
2019-04-18 03:46:38 +0200

    Proyecto Aplicacion 19 -Añadir el repositorio

--- view/layout.php
+++ view/layout.php
@@ -61,12 +61,12 @@ if (isset($_SESSION['pagina'])) {
                         <h4 class="text-uppercase mb-4">Repositorio</h4>
                         <ul class="list-inline mb-0">
                             <li class="list-inline-item">
-                                <a class="btn btn-outline-light btn-social text-center rounded-circle" href="https://github.com/laura1819/ProyectoAplicacion19.git">
+                                <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#">
                                     <i class="fab fa-gitlab"></i>
                                 </a>
                             </li>
                             <li class="list-inline-item">
-                                <a class="btn btn-outline-light btn-social text-center rounded-circle" href="https://github.com/laura1819/ProyectoAplicacion19">
+                                <a class="btn btn-outline-light btn-social text-center rounded-circle" href="https://github.com/laura1819/ProyectoAplicacion19" target="blank">
                                     <i class="fab fa-github"></i>
                                 </a>
                             </li>
